01439db5503230e18644a394bd437beea7fa7c80
Aqshola <aqsholafid2@gmail.com>
2021-09-29 13:41:52 +0700

    fail

---
+++ client/src/components/Button.stories.ts
@@ -0,0 +1,10 @@
+import React,{ComponentProps} from 'react'
+import { Story, Meta } from '@storybook/react'
+import Button from './Button'
+
+
+export default {
+    title: "Button",
+    component: Button
+    
+}as Meta
---
+++ client/src/components/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button() {
+    return (
+        <button className="bg-red-400 p-4">
+            Test Button
+        </button>
+    )
+}
