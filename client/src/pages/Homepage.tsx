f39ab54bf5df8a1e05ada080757b70275cfb2fc9
Aqshola <aqsholafid2@gmail.com>
2021-10-07 20:24:05 +0700

    fix homepage layout

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -7,12 +7,12 @@ interface Props {}
 
 export default function Homepage({ ...props }: Props): ReactElement {
   return (
-    <div className="w-screen min-h-screen max-w-screen-xs mx-auto border-0 md:border lg:border-2">
+    <div className="min-h-screen  border-0 md:border lg:border-2">
       <Nav />
       <div className="mt-3 flex  justify-center">
         <SearchBar placeholder="Search Book" className="w-64" />
       </div>
-      <div className="flex flex-col space-y-10 items-center mt-5 w-full px-5">
+      <div className="flex flex-col space-y-10 py-5 items-center mt-5 mx-auto px-5 w-screen max-w-screen-xs">
         <CardBook
           title="The book of Wanderer"
           genre={['Adventure', 'Drama']}
